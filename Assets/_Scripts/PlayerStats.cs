06ea88682a34ed3c3097b82a9260d893ee150b1f
Zsef <114775729+Balslev05@users.noreply.github.com>
2024-10-22 14:34:28 +0200

    Changes_Item

---
+++ Assets/_Scripts/PlayerStats.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [Header("Stats")]
+    public int maxHealth = 100;
+    public int currentHealth;
+    public int AttackSpeed;
+    public int AttackDamage;
+    public int critChance;
+    public int critDamage;
+    public int blockChance;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
